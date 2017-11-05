f916f2db39eb0ec6858d803b8811ab2cbb8bec95
eminence <eminence@localhost.localdomain>
2017-11-05 23:00:55 +0800

    view test done

--- ZHtelecom/login/views.py
+++ ZHtelecom/login/views.py
@@ -1,3 +1,11 @@
+#coding:utf-8
+#from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import RequestContext,loader
 
 # Create your views here.
+def loginui(request):
+    # tmp=loader.get_template('login/login.html')
+    # return HttpResponse(tmp.render())
+    return render(request,'login/login.html')
+
